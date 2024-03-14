e70485af4167dce2025db5bb3e82367b441855be
anshmaurya2908 <anshmaurya2908@gmail.com>
2024-03-15 00:00:33 +0530

    commit

---
+++ build/precache-manifest.8496c2fd22b132e5942fba3335c567f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea45ce147f9a96bdbb5ccff0feacc6b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe5d89e259a6b204b705",
+    "url": "/static/css/main.ddeefc5a.chunk.css"
+  },
+  {
+    "revision": "e08dbf7ab17d7361be74",
+    "url": "/static/js/2.98aee11a.chunk.js"
+  },
+  {
+    "revision": "2c10491364e9bdf48063",
+    "url": "/static/js/3.fc5cbf83.chunk.js"
+  },
+  {
+    "revision": "fe5d89e259a6b204b705",
+    "url": "/static/js/main.1adcefd0.chunk.js"
+  },
+  {
+    "revision": "a50475a1f486eef27c5a",
+    "url": "/static/js/runtime~main.4414e1f1.js"
+  }
+]);
---
+++ index.js
@@ -0,0 +1,79 @@
+require('dotenv').config()
+const express=require('express');
+const morgan = require('morgan');
+const Person=require("./models/person");
+const cors=require("cors");
+const app = express(); 
+app.use(express.json());
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.static('build'));
+app.get('/',(req,res)=>{
+    res.send('Hello World!');
+})
+app.get('/info', (req, res) =>{
+  Person.countDocuments({})
+  .then(count=>{
+  res.send(`PhoneBook has ${count}  contacts </br> ${Date()}`);
+  })
+})
+app.get('/api/persons',(req,res)=>{
+  Person.find({}).then(persons=>{
+    res.json(persons);
+})
+})
+app.get('/api/persons/:id',(req,res,next)=>{
+      Person.findById(req.params.id)
+      .then(person=>{
+           res.json(person);
+      })
+      .catch(error=>next(error));
+})
+app.post('/api/persons',(req,res)=>{
+    const body=req.body;
+    if (body.name === undefined) {
+    return response.status(400).json({ error: 'name missing' })
+  }
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
+  person.save().then(savedPerson => {
+    res.json(savedPerson)
+  })
+})
+app.put('/api/persons/:id',(req,res,next)=>{
+  const body=req.body;
+  const person={
+    name:body.name,
+    number:body.number
+  }
+  console.log('updating')
+  Person.findByIdAndUpdate(req.params.id,person,{new:true})
+  .then(updatedPerson=>{
+    res.json(updatedPerson);
+  })
+  .catch(error=>next(error));
+})
+app.delete('/api/persons/:id',(req,res,next)=>{
+  Person.findByIdAndDelete(req.params.id)
+      .then(person=>{
+          res.status(200).end('deleted');
+      })
+    .catch(error=>next(error))
+})
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+const errorHandler=(error,req,res,next)=> {
+  if(error.name==='CastError'){
+    return res.status(400).send('castError');
+  }
+  next(error);
+}
+app.use(errorHandler);
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
